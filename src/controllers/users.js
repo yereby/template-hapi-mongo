e8c4669f812c6a4748b22d68381edd82d9c541f7
Jérémy Giuseppi <yereby@gmail.com>
2017-11-28 01:32:35 +0100

    Use return instead of reply, findOneAnd…, add set method and format for hapi 17

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -2,47 +2,69 @@ const Boom = require('boom')
 const Joi = require('joi')
 const User = require('../models/users')
 
-module.exports.list = (request, reply) => {
-  User.find({})
+module.exports.list = () => {
+  return User.find({})
     .then(users => {
-      if (!users.length) { return reply(Boom.notFound()) }
+      if (!users.length) { return Boom.notFound() }
 
-      return reply(users)
+      return users
     })
-    .catch(err => reply(Boom.badImplementation(err)))
+    .catch(err => Boom.badImplementation(err))
 }
 
-module.exports.signup = {
+module.exports.create = {
   validate: {
     payload: {
       email: Joi.string().email().required(),
       name: Joi.string().required()
     }
   },
-  handler: (request, reply) => {
-    User.create(request.payload)
-      .then(reply)
+  handler: request => {
+    return User.create(request.payload)
+      .then()
       .catch(err => {
         switch (err.code) {
         case 11000:
         case 11001:
-          reply(Boom.conflict(err))
-          break
+          return Boom.conflict(err)
         default:
-          reply(Boom.forbidden(err))
+          return Boom.forbidden(err)
         }
       })
   }
 }
 
-module.exports.remove = (request, reply) => {
-  User.remove({ _id: request.params.id })
-    .then(response => {
-      if (response.result && response.result.n === 0) {
-        return reply(Boom.notFound())
-      }
+module.exports.remove = request => {
+  return User.findOneAndRemove({ _id: request.params.id })
+    .then(res => {
+      if (!res) { return Boom.notFound() }
 
-      return reply()
+      return res
     })
-    .catch(err => reply(Boom.badImplementation(err)))
+    .catch(err => Boom.badImplementation(err))
+}
+
+module.exports.set = {
+  validate: {
+    payload: {
+      name: Joi.string().required()
+    }
+  },
+  handler: request => {
+    return User.findOneAndUpdate({ _id: request.params.id }, request.payload, { new: true})
+      .then(res => {
+        if (!res) { return Boom.notFound() }
+
+        return res
+      })
+      .catch(err => {
+        switch (err.code) {
+        case 11000:
+        case 11001:
+          return Boom.conflict(err)
+        default:
+          return Boom.forbidden(err)
+        }
+      })
+  }
 }
