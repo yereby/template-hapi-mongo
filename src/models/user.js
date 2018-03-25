3160fe5e35eb976aefcb1252aaa3cddcc1702ab5
Jérémy Giuseppi <yereby@gmail.com>
2018-03-25 16:23:00 +0200

    Revert "Remove pre save model"
    This reverts commit 464bfde00954d08b8e85622f0dc41ee58dee2f83.

--- src/models/user.js
+++ src/models/user.js
@@ -19,5 +19,39 @@ const userSchema = new Mongoose.Schema({
   scope: { type: Array, default: ['user'] },
 })
 
+/**
+ * Ensure the scope is filled only with accepted and unique values
+ * And set the default scope
+ *
+ * @param {Array|string} val The value to check
+ * @return {Array} The scope formated
+ */
+function checkScope(scope) {
+  const defaultValue = ['user']
+  const acceptedValues = ['user', 'admin']
+
+  if (!scope || !scope.length) { return defaultValue }
+
+  // Check if the array contains anly values in the values array
+  scope = scope.filter(item =>
+    acceptedValues.findIndex(x => x === item) !== -1
+  )
+
+  // Return an array with unique values
+  return [...new Set(scope)]
+}
+
+// ## Pre Methods
+
+userSchema.pre('save', true, function (next, done) {
+  this.scope = checkScope(this.scope)
+  next(done())
+})
+
+userSchema.pre('findOneAndUpdate', true, function (next, done) {
+  this._update.scope = checkScope(this._update.scope)
+  next(done())
+})
+
 module.exports = Mongoose.model('User', userSchema)
 
